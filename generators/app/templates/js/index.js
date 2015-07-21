c10a4425e4e4548a521654779525f14272ff8939
Ben Anderson <banderson@hubspot.com>
2015-07-21 00:48:00 -0400

    Clean up conditional require

--- generators/app/templates/js/index.js
+++ generators/app/templates/js/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-// TODO: better way to do this?
-const App = require(
-  __DEV__
-  ? './containers/AppWithDevTools'
-  : './containers/App'
-);
+let App;
+if (__DEV__) {
+  App = require('./containers/AppWithDevTools');
+} else {
+  App = require('./containers/App');
+}
 
 React.render(<App />, document.getElementById('main'));
