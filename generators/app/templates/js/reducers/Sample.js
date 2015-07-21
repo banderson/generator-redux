c0ff35db8bd4b7b87aef94e963664e977d37f527
Ben Anderson <banderson@hubspot.com>
2015-07-20 20:15:48 -0400

    Sample reducer

---
+++ generators/app/templates/js/reducers/Sample.js
@@ -0,0 +1,10 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+let defaultState = [];
+
+export default function(state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ generators/app/templates/js/reducers/index.js
@@ -0,0 +1,1 @@
+export { default as Sample } from './Sample';
