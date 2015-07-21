511991ad3f9c43507af786ef6945c146795be62c
Ben Anderson <banderson@hubspot.com>
2015-07-20 20:16:27 -0400

    Include JSX loader

--- generators/app/index.js
+++ generators/app/index.js
@@ -37,7 +37,7 @@ module.exports = yeoman.generators.Base.extend({
 
     buildTools: function() {
       this.npmInstall([
-        'webpack', 'webpack-dev-server', 'css-loader',
+        'webpack', 'webpack-dev-server', 'css-loader', 'jsx-loader',
         'babel-core', 'babel-loader', 'react-hot-loader'
       ], {'saveDev': true });
     },
