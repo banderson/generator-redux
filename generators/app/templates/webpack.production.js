b266d9a696e210fec298071a5e1bc3d5fa22c472
Peter Fern <github@obfusc8.org>
2015-09-05 01:37:09 +1000

    Don't include development libs when building for production
    Add a production webpack config and use it for the `build` script.  Add
    an additional script `build-dev` for building static assets that include
    the hot-loader.
    
    Fixes #9

---
+++ generators/app/templates/webpack.production.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var devFlagPlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
+
+module.exports = {
+  entry: [
+    './js/index.js'
+  ],
+  output: {
+    path: __dirname + '/static/',
+    publicPath: '/static/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    devFlagPlugin,
+    new ExtractTextPlugin('app.css')
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+};
